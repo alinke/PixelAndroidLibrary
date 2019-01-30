0dc5e32979330e7ff8f521638c7cfd4164871073
Alfred Linke <alinke@yahoo.com>
2019-01-29 23:50:34 -0800

    First commit

---
+++ pixelHelloWorld/build/generated/source/buildConfig/debug/com/ledpixelart/pixelhelloworld/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.ledpixelart.pixelhelloworld;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.ledpixelart.pixelhelloworld";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 10;
+  public static final String VERSION_NAME = "1.5";
+}
